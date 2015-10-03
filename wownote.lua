91d274eb6488c7249106ab9728389fdbde7253b5
jsRuner <hi_php@163.com>
2015-10-03 21:03:08 +0800

    获取状态
    获取fps 延迟和金钱数

--- wownote.lua
+++ wownote.lua
@@ -15,35 +15,36 @@ function HelloWorldEvent()
    end 
 end
 
+--对象
 myFrame = getglobal("HelloWorldTestFrame"); 
 
 
 
 
--- function HelloWorldCommand(cmd) 
---    myFrame = getglobal("HelloWorldTestFrame"); 
---    if(not myFrame:IsShown()) then 
---       myFrame:Show(); 
---    else 
---       myFrame:Hide(); 
---    end 
--- end 
+function HelloWorldCommand(cmd) 
+   myFrame = getglobal("HelloWorldTestFrame"); 
+   if(not myFrame:IsShown()) then 
+      myFrame:Show(); 
+   else 
+      myFrame:Hide(); 
+   end 
+end 
 
--- function HelloWorldLoad() 
---    getglobal("HelloWorldTestFrame"):Hide(); 
---    DEFAULT_CHAT_FRAME:AddMessage("HelloWorld is Loaded!"); 
---    SLASH_HELLOWORLD1 = "/helloworld"; 
---    SLASH_HELLOWORLD2 = "/hw"; 
---    SlashCmdList["HELLOWORLD"] = HelloWorldCommand; 
--- end 
+function HelloWorldLoad() 
+   getglobal("HelloWorldTestFrame"):Hide(); 
+   DEFAULT_CHAT_FRAME:AddMessage("HelloWorld is Loaded!"); 
+   SLASH_HELLOWORLD1 = "/helloworld"; 
+   SLASH_HELLOWORLD2 = "/hw"; 
+   SlashCmdList["HELLOWORLD"] = HelloWorldCommand; 
+end 
 
--- function HelloWorldFrameUpdate() 
---    textFPS = getglobal("HelloWorldTestFrameTextFPS"); 
---    textDelay = getglobal("HelloWorldTestFrameTextDelay"); 
---    textMoney = getglobal("HelloWorldTestFrameTextMoney"); 
---    down, up, lag = GetNetStats(); 
+function HelloWorldFrameUpdate() 
+   textFPS = getglobal("HelloWorldTestFrameTextFPS"); 
+   textDelay = getglobal("HelloWorldTestFrameTextDelay"); 
+   textMoney = getglobal("HelloWorldTestFrameTextMoney"); 
+   down, up, lag = GetNetStats(); 
 
---    textFPS:SetText("FPS   "..floor(GetFramerate())); 
---    textDelay:SetText("Delay   "..lag.." ms"); 
---    textMoney:SetText("Money   "..floor(GetMoney()/10000).." G"); 
--- end
+   textFPS:SetText("FPS   "..floor(GetFramerate())); 
+   textDelay:SetText("Delay   "..lag.." ms"); 
+   textMoney:SetText("Money   "..floor(GetMoney()/10000).." G"); 
+end
